ac9e85a7400b976d2f9686dd939b5b15c74c875a
avk <alvanklaveren@gmaill.com>
2020-04-17 11:50:06 +0200

    second commit in setup phase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'avk-fe';
+  errormessage: string = null;
+  successmessage: string = "website is working";
+
+  onErrorMessageClick(){
+    this.errormessage = null;
+  }
+
+  onSuccesMessageClick(){
+    this.successmessage = null;
+  }
 }
