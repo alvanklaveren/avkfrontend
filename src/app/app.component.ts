3b13bd43b838de9934b29ac7f872f01d77e782bd
avk <alvanklaveren@gmaill.com>
2020-04-18 16:03:50 +0200

    about website text pulled from backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslationService } from './services/translation.service';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,24 @@ export class AppComponent implements OnInit{
   title = 'AVK';
   errormessage: string = null;
   successmessage: string = null;
-
-  constructor(private router:Router){ }
+  about: string = 'About ...';
+  aboutWebsite: string = '[aboutwebsite]';
+  
+  constructor(private router:Router, private translationService: TranslationService){ }
 
   ngOnInit(){
+    this.translationService.translate(this.about, "US").subscribe(res => {
+      let response = res as any;
+      console.log(response.result);
+      this.about = response.result as string;
+    });
+
+    this.translationService.translate(this.aboutWebsite, "US").subscribe(res => {
+      let response = res as any;
+      console.log(response.result);
+      this.aboutWebsite = response.result as string;
+    });
+
     this.router.navigateByUrl('home');
   }
 
---
+++ src/app/services/translation.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class TranslationService {
+
+  constructor(private http: HttpClient) {}
+
+  translate(original: string, isoA2?: string) {
+    return this.http.post(environment.backendUrl + 'translation/translate', {original: original, isoA2: isoA2});
+  }
+
+}
