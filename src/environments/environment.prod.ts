919f834dc4bf51f96ff3da248aac94d030029edb
avk <alvanklaveren@gmail.com>
2022-07-23 09:24:33 +0200

    dark mode menu black

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   mailTo: 'info@alvanklaveren.com',
-  backendUrl: 'https://alvanklaveren.com:444/backend/' // = 'https://82.196.9.141:444/backend/'
+  backendUrl: 'https://www.alvanklaveren.com:8444/backend/'
 
 };
