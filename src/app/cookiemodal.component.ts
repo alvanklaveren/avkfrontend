c36e1a4cc7ba5dbd2937af237f9d098c0955208a
avk <alvanklaveren@gmail.com>
2024-08-14 12:21:23 +0200

    cookie hell #3

--- src/app/cookiemodal.component.ts
+++ src/app/cookiemodal.component.ts
@@ -10,12 +10,11 @@ import { ContextService } from './services/context.service';
 export class CookieModalComponent implements OnInit{
 
     selectedIsoA2 = this.contextService.getIsoA2();
-    cookiemessage: string = '<b>Cookies</b><br>This site only uses technical cookies that are necessary for this site to function. By continuing ' + 
-    'to use this site you accept the use of these cookies on your device. If you disabled cookies in your browser settings, ' +
-    'this site may not function properly. <a href="http://cookiesandyou.com/" target="_blank">Learn more about cookies.</a> ' +
-    '<br>';
-;
-    okButton: string = 'Understood';
+    // cookiemessage: string = '<b>Cookies</b><br>This site only uses technical cookies that are necessary for this site to function. By continuing ' + 
+    // 'to use this site you accept the use of these cookies on your device. If you disabled cookies in your browser settings, ' +
+    // 'this site may not function properly. <a href="http://cookiesandyou.com/" target="_blank">Learn more about cookies.</a> ' +
+    // '<br>';
+    // okButton: string = 'Understood';
 
     constructor(private modalService: NgbModal, private activeModal: NgbActiveModal,
                 private contextService: ContextService) { 
@@ -23,15 +22,15 @@ export class CookieModalComponent implements OnInit{
     }
 
     ngOnInit(){
-      if(this.selectedIsoA2.toUpperCase() === 'NL'){
-        this.cookiemessage = '<b>Cookies</b><br>Deze website gebruikt enkel technische cookies die nodig zijn om deze website goed te laten functioneren. ' + 
-        'Bij verder gebruik van deze website ben je dus op de hoogte dat dit soort cookies op dit apparaat zullen worden gebruikt. ' +
-        'Als je cookies hebt uitgeschakeld in je browser, dan zal deze website mogelijk minder goed functioneren. ' +
-        '<a href="http://cookiesandyou.com/" target="_blank">Leer meer over cookies.</a> ' +
-        '<br>';
+      // if(this.selectedIsoA2.toUpperCase() === 'NL'){
+      //   this.cookiemessage = '<b>Cookies</b><br>Deze website gebruikt enkel technische cookies die nodig zijn om deze website goed te laten functioneren. ' + 
+      //   'Bij verder gebruik van deze website ben je dus op de hoogte dat dit soort cookies op dit apparaat zullen worden gebruikt. ' +
+      //   'Als je cookies hebt uitgeschakeld in je browser, dan zal deze website mogelijk minder goed functioneren. ' +
+      //   '<a href="http://cookiesandyou.com/" target="_blank">Leer meer over cookies.</a> ' +
+      //   '<br>';
 
-        this.okButton = "Begrepen";
-      }     
+      //   this.okButton = "Begrepen";
+      // }     
     }
 
  
