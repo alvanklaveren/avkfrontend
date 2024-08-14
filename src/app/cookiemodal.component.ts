11b6196ae0d3b4fee311555501cae08a0c9fe6cb
avk <alvanklaveren@gmail.com>
2024-08-14 11:51:50 +0200

    move init to constructor

--- src/app/cookiemodal.component.ts
+++ src/app/cookiemodal.component.ts
@@ -14,26 +14,29 @@ export class CookieModalComponent implements OnInit{
     okButton: string;
 
     constructor(private modalService: NgbModal, private activeModal: NgbActiveModal,
-                private contextService: ContextService) { }
+                private contextService: ContextService) { 
+
+      if(this.selectedIsoA2.toUpperCase() === 'NL'){
+        this.cookiemessage = '<b>Cookies</b><br>Deze website gebruikt enkel technische cookies die nodig zijn om deze website goed te laten functioneren. ' + 
+        'Bij verder gebruik van deze website ben je dus op de hoogte dat dit soort cookies op dit apparaat zullen worden gebruikt. ' +
+        'Als je cookies hebt uitgeschakeld in je browser, dan zal deze website mogelijk minder goed functioneren. ' +
+        '<a href="http://cookiesandyou.com/" target="_blank">Leer meer over cookies.</a> ' +
+        '<br>';
+
+        this.okButton = "Begrepen";
+      } else {
+        this.cookiemessage = '<b>Cookies</b><br>This site only uses technical cookies that are necessary for this site to function. By continuing ' + 
+        'to use this site you accept the use of these cookies on your device. If you disabled cookies in your browser settings, ' +
+        'this site may not function properly. <a href="http://cookiesandyou.com/" target="_blank">Learn more about cookies.</a> ' +
+        '<br>';
+
+        this.okButton = "Understood";
+      }
+
+    }
 
     ngOnInit(){
       
-          if(this.selectedIsoA2.toUpperCase() === 'NL'){
-            this.cookiemessage = '<b>Cookies</b><br>Deze website gebruikt enkel technische cookies die nodig zijn om deze website goed te laten functioneren. ' + 
-            'Bij verder gebruik van deze website ben je dus op de hoogte dat dit soort cookies op dit apparaat zullen worden gebruikt. ' +
-            'Als je cookies hebt uitgeschakeld in je browser, dan zal deze website mogelijk minder goed functioneren. ' +
-            '<a href="http://cookiesandyou.com/" target="_blank">Leer meer over cookies.</a> ' +
-            '<br>';
-
-            this.okButton = "Begrepen";
-          } else {
-            this.cookiemessage = '<b>Cookies</b><br>This site only uses technical cookies that are necessary for this site to function. By continuing ' + 
-            'to use this site you accept the use of these cookies on your device. If you disabled cookies in your browser settings, ' +
-            'this site may not function properly. <a href="http://cookiesandyou.com/" target="_blank">Learn more about cookies.</a> ' +
-            '<br>';
-
-            this.okButton = "Understood";
-          }
     }
 
  
